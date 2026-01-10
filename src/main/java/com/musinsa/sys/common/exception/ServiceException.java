b61633d89ebe76d022fde69436bb78a3dc82bfce
2jisu6264 <2jisu6264@naver.com>
2026-01-10 22:17:29 +0900

    commit_

--- src/main/java/com/musinsa/sys/common/exception/ServiceException.java
+++ src/main/java/com/musinsa/sys/common/exception/ServiceException.java
@@ -7,14 +7,7 @@ public class ServiceException extends RuntimeException {
 
     private final String procCd;
 
-    private String apnMsg;
-
     public ServiceException(String inProcCd) {
         this.procCd = inProcCd;
-    }
-
-    public ServiceException(String inProcCd, String apnMsg) {
-        this.procCd = inProcCd;
-        this.apnMsg = apnMsg;
     }
 }
